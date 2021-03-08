eaa3c70209da1c9cfb38ccdf605f6ea81821c884
Noah Heague <noahheague@gmail.com>
2021-03-08 13:09:31 +1100

    Fucking js 32 bit number type smh

--- src/sixBit/sixbit.spec.ts
+++ src/sixBit/sixbit.spec.ts
@@ -4,11 +4,11 @@ import {pack, unpack} from "./sixbit";
 
 describe('Six-bit pack/unpack', () => {
   it('should pack a simple string', () => {
-    const result = pack('ABC')
-    assert.deepEqual(result, Buffer.from('2cc340', 'hex'))
+    const result = pack('kbinXML')
+    assert.deepEqual(result, Buffer.from('c27bb3897580', 'hex'))
   })
   it('should unpack to a string', () => {
-    const result = unpack(Buffer.from('2cc340', 'hex'))
-    assert.deepEqual(result, 'ABC')
+    const result = unpack(Buffer.from('c27bb3897580', 'hex'))
+    assert.deepEqual(result, 'kbinXML')
   })
 })
--- src/sixBit/sixbit.ts
+++ src/sixBit/sixbit.ts
@@ -1,3 +1,6 @@
+import { bigintToBuf } from 'bigint-conversion'
+
+// TODO: Make this more readable, It functions but it could be confusing.
 const CHAR_MAP = '0123456789:ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
 const BYTE_MAP: {[key: string]: number} = CHAR_MAP.split("").reduce((acc, val) => {
   acc[val] = CHAR_MAP.split("").indexOf(val)
@@ -5,28 +8,46 @@ const BYTE_MAP: {[key: string]: number} = CHAR_MAP.split("").reduce((acc, val) =
 }, {})
 
 export const unpack = (compactBuffer: Buffer): string => {
-  return "ABC"
+  const length = compactBuffer.length
+  const lengthInBits = length * 6
+  const lengthInBytes = Math.floor((lengthInBits + 7) / 8)
+
+  // Calculate padding and fallback to 0 if its equal to 8
+  const calcPadding = 8 - (lengthInBits % 8)
+  const padding = calcPadding === 8 ? 0 : calcPadding
+
+  let bits = compactBuffer.readIntBE(0, lengthInBytes)
+  bits <<= padding;
+  let result = [];
+
+  // Just a nice ES6 way to do something n times
+  [...Array(length)].forEach(() => {
+    result.push(bits & 0b111111)
+    bits >>= 6
+  })
+
+  return result.map(x => CHAR_MAP[x]).join("")
 }
 
 export const pack = (string: string): any => {
   const chars: number[] = string.split("").map(char => BYTE_MAP[char])
 
-  const stringPadding = 8 - ((string.length)*6 % 8)
-  const padding = stringPadding === 8 ? 0 : stringPadding
+  // Calculate padding and fallback to 0 if its equal to 8
+  const calcPadding = 8 - ((string.length)*6 % 8)
+  const padding = calcPadding === 8 ? 0 : calcPadding
 
-  let bits = 0;
+  // Javascript represents all numbers as 64-bit double precision IEEE 754 floating point numbers
+  // We need a 64 bit integer here!
+  let bits = BigInt(0);
 
   chars.forEach(char => {
-    bits <<= 6; // Shift bits left 6
-    bits |= char // OR bitwise operator
+    const bigChar = BigInt(char)
+    bits = bits <<= BigInt(6)
+    bits = bits | bigChar // OR bitwise operator
   })
 
-  bits <<= padding;
+  bits <<= BigInt(padding);
 
-  const length = Math.floor((string.length*6 + padding) / 8);
-
-  const buf = Buffer.alloc(length)
-  buf.writeIntBE(bits, 0, length)
-
-  return buf
+  // TODO: I could not figure this out myself, Replace this library eventually!
+  return bigintToBuf(bits)
 }
